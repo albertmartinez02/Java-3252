d1fadae09a7d003879378e60313be08e139fb7f4
albertmartinez02 <110438010+albertmartinez02@users.noreply.github.com>
2023-02-05 12:49:16 -0500

    Create product.java
    Assignment : Using a variable length argument list, calculate product of series of ints that are passed to method product.

---
+++ February7Assignments/product.java
@@ -0,0 +1,23 @@
+public class product{
+    public static int result(int... numbers){
+     int x = 1;
+       for(int y : numbers){
+        x *= y;
+       }
+       return x;
+    }
+ public static void main(String[] args){
+    int[] array = {11,22,33,44,55,66};
+    int x = result(array);
+    System.out.printf( "%s%05d%n","First call to product function with input array yields: ", x);
+
+    int[] array2 = {1,2,3,4,5,6,7,8};
+    int y = result(array2);
+    System.out.printf( "%s%05d%n","Second call to product function with input array2 yields: ", y);
+
+    int[] array3 = {1,1,2,3,5,8,13,21,34};
+    int z = result(array3);
+    System.out.printf( "%s%05d%n","Third call to product function with input array3 yields: ", z);
+
+ }
+}
